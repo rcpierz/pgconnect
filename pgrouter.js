82ba5bcc486e94cbbf483497bafb1c81efa0727c
RCP <rcpierz@gmail.com>
2019-09-16 16:02:49 -0500

    Added ESLint and auto-fixed code issues

--- pgrouter.js
+++ pgrouter.js
@@ -1,38 +1,38 @@
-var express = require('express');
-var router = express.Router();
-var pgservice = require('./pgservice.js');
+const express = require('express');
+const router = express.Router();
+const pgservice = require('./pgservice.js');
 
 
-router.get('/', (req,res) =>{
-	console.log('[ROUT] GET /');
-	res.send('Base path (\'/\') accessed with GET');
+router.get('/', (req, res) =>{
+  console.log('[ROUT] GET /');
+  res.send('Base path (\'/\') accessed with GET');
 });
 
-router.get('/about',(req,res)=>{
-	console.log('[ROUT] GET /about');
-	res.send('About page')
+router.get('/about', (req, res)=>{
+  console.log('[ROUT] GET /about');
+  res.send('About page');
 });
 
-router.get('/test',(req,res) => {
-	console.log('[ROUT] GET /test');
-	pgservice.testLog();
-	res.send('Test Endpoint reached');
-})
-
-router.get('/entries/:column', (req,res)=>{
-	console.log('[ROUT] GET /entries/'+req.params.column);
-	pgservice.searchByColumn(req.params.column, (output) => res.send(output));
-})
-
-
-router.post('/entries', (req,res) => {
-	console.log('[ROUT] POST /entries');
-	pgservice.addEntry(req, () => res.send('201 CREATED'));
+router.get('/test', (req, res) => {
+  console.log('[ROUT] GET /test');
+  pgservice.testLog();
+  res.send('Test Endpoint reached');
 });
 
-router.delete('/entries/:id', (req,res) => {
-	console.log('[ROUT] DELETE /entries/'+req.params.id);
-	pgservice.deleteEntry(req.params.id, () => res.send('202 REQUEST RECEIVED'));	
+router.get('/entries/:column', (req, res)=>{
+  console.log('[ROUT] GET /entries/'+req.params.column);
+  pgservice.searchByColumn(req.params.column, (output) => res.send(output));
 });
 
-module.exports = router
+
+router.post('/entries', (req, res) => {
+  console.log('[ROUT] POST /entries');
+  pgservice.addEntry(req, () => res.send('201 CREATED'));
+});
+
+router.delete('/entries/:id', (req, res) => {
+  console.log('[ROUT] DELETE /entries/'+req.params.id);
+  pgservice.deleteEntry(req.params.id, () => res.send('202 REQUEST RECEIVED'));
+});
+
+module.exports = router;
--- pgservice.js
+++ pgservice.js
@@ -1,36 +1,37 @@
-const { Pool, Client } = require('pg');
+const {Client} = require('pg');
 
 const client = new Client({
-	user: 'postgres',
-	host: 'localhost',
-	database: 'generaldb',
-	password: 'postgres',
-	port: 5432
+  user: 'postgres',
+  host: 'localhost',
+  database: 'generaldb',
+  password: 'postgres',
+  port: 5432,
 });
 
 client.connect();
 console.log('[SRVC] Database connection is established');
 
 exports.testLog = function() {
-	console.log('[SRVC] running testLog function');
+  console.log('[SRVC] running testLog function');
 };
 
 exports.searchByColumn = function(column, callback) {
-	console.log('[SRVC] searching dailyexpense table with criterion column = '+column);
-	client.query('SELECT '+column+' FROM dailyexpense;')
-		.then( output => {callback(output.rows)});
-		
+  console.log('[SRVC] searching dailyexpense table with criterion column = '+column);
+  client.query('SELECT '+column+' FROM dailyexpense;')
+      .then( (output) => {
+        callback(output.rows);
+      });
 };
 
 exports.addEntry = function(req, callback) {
-	console.log('[SRVC] adding a new entry to dailyexpense table with body = '+JSON.stringify(req.body));
-	client.query('INSERT INTO dailyexpense VALUES('+req.body.id+',\''+req.body.date+'\',\''+req.body.meal+'\',\''+req.body.source+'\',\''+req.body.item+'\','+req.body.cost+');')
-		.then( () => callback());
+  console.log('[SRVC] adding a new entry to dailyexpense table with body = '+JSON.stringify(req.body));
+  client.query('INSERT INTO dailyexpense VALUES('+req.body.id+',\''+req.body.date+'\',\''+req.body.meal+'\',\''+req.body.source+'\',\''+req.body.item+'\','+req.body.cost+');')
+      .then( () => callback());
 };
 
 exports.deleteEntry = function(id, callback) {
-	console.log('[SRVC] deleting entries with id = '+id);
-	client.query('DELETE FROM dailyexpense WHERE id = '+id)
-		.then( () => callback());
-}
+  console.log('[SRVC] deleting entries with id = '+id);
+  client.query('DELETE FROM dailyexpense WHERE id = '+id)
+      .then( () => callback());
+};
 
